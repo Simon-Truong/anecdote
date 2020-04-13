4aa2ae44def6592a090d204f709296131779b623
Simon Truong <me.simon_@hotmail.com>
2020-04-13 13:06:01 +1000

    [feat] implemented query into get users

--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,25 @@ const pool = new Pool({
 
 router.get("/users", async (req, res) => {
   try {
-    const result = (await pool.query("SELECT * FROM users")).rows;
+    const { q } = req.query;
+
+    const pgQuery = `
+      SELECT DISTINCT id, first_name, surname, tags
+      FROM (SELECT id, first_name, surname, tags, unnest(tags) AS unnestedTags
+        FROM public.users) x
+      WHERE 
+      lower(first_name) LIKE $1 OR
+      lower(surname) LIKE $1 OR
+      lower(unnestedTags) LIKE $1
+    `;
+
+    const result = (await pool.query(pgQuery, [`%${q}%`])).rows;
 
     res.send(result);
   } catch (error) {
     console.log({ error });
+
+    res.sendStatus(500);
   }
 });
 
