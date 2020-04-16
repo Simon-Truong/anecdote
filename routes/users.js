0db8078d42ec3b9ec9cd46581310d6907307b6e7
Simon Truong <simontruong2@gmail.com>
2020-04-16 21:58:05 +1000

    [feat] added bcrypt, uuid and initial implementation of sign up

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const { Pool } = require("pg");
+const bcrypt = require("bcrypt");
+const uuid = require("uuid");
 const validation = require("../middleware/validation");
+const { Pool } = require("pg");
 
 const router = express.Router();
 
@@ -14,28 +16,82 @@ router.get("/users", async (req, res) => {
   try {
     const { q } = req.query;
 
-    const pgQuery = `
-      SELECT DISTINCT id, first_name, surname, tags
-      FROM (SELECT id, first_name, surname, tags, unnest(tags) AS unnestedTags
-        FROM public.users) x
-      WHERE 
-      lower(first_name) LIKE $1 OR
-      lower(surname) LIKE $1 OR
-      lower(unnestedTags) LIKE $1
-    `;
-
-    const result = (await pool.query(pgQuery, [`%${q}%`])).rows;
+    result = await getUsers(q);
 
     res.send(result);
   } catch (error) {
     console.log({ error });
 
-    res.sendStatus(400);
+    res.status(400).send(error);
   }
 });
 
-router.post("/user", validation, (req, res) => {
-  
+router.post("/user", validation, async (req, res) => {
+  const { body } = req;
+
+  try {
+    var existentUser = await getUserByEmail(body.email);
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+
+  if (existentUser.length) {
+    return res.status(400).send("Email is already in use.");
+  }
+
+  const SALT_ROUNDS = 10;
+
+  bcrypt.hash(body.password, SALT_ROUNDS, async (error, hash) => {
+    if (error) {
+      return res.status(400);
+    }
+
+    const newUser = body;
+    newUser.password = hash;
+
+    try {
+      var result = await createUser(newUser);
+    } catch (error) {
+      return res.status(400).send(error);
+    }
+
+    console.log({ result });
+  });
 });
+
+async function getUsers(query) {
+  const pgQuery = `
+    SELECT DISTINCT id, first_name, surname, tags
+    FROM (SELECT id, first_name, surname, tags, unnest(tags) AS unnestedTags
+      FROM ${process.env.TABLE}) x
+    WHERE 
+    lower(first_name) LIKE $1 OR
+    lower(surname) LIKE $1 OR
+    lower(unnestedTags) LIKE $1
+  `;
+
+  return (await pool.query(pgQuery, [`%${query}%`])).rows;
+}
+
+async function getUserByEmail(email) {
+  const pgQuery = `
+    SELECT id
+    FROM ${process.env.TABLE}
+    WHERE email = $1
+  `;
+
+  return (await pool.query(pgQuery, [email])).rows;
+}
+
+async function createUser(newUser) {
+  const { firstName, surname, email, password, joined } = newUser;
+
+  const pgQuery = `
+    INSERT INTO ${process.env.TABLE} (id, first_name, surname, email, password, joined)
+    VALUES ($1, $2, $3, $4, $5, $6)
+  `;
+
+  return await pool.query(pgQuery, [uuid.v4(), firstName, surname, email, password, joined]);
+}
 
 module.exports = router;
