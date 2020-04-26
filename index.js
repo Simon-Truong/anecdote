e6243c4c231d609653dc9001cffb7d3c482b638b
Simon Truong <simontruong2@gmail.com>
2020-04-26 10:39:48 +1000

    [feat] added helmet

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ require('dotenv').config();
 require('./authentication/passport-strategy');
 
 const express = require('express');
+const port = process.env.PORT || 3000;
 const app = express();
 const passport = require('passport');
 const morgan = require('morgan');
-const port = process.env.PORT || 3000;
+const helmet = require('helmet');
 
 const cors = require('cors');
 
@@ -15,6 +16,7 @@ const cors = require('cors');
 const users = require('./routes/users');
 
 // middle ware
+app.use(helmet());
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
